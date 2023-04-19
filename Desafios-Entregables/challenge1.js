09f685a91c906bec1a54313aeb611a0d808cdd77
Soft <brunoulloaduran90@gmail.com>
2023-04-19 06:15:15 -0300

    Added explaining comments in `challenge1.js`

--- Desafios-Entregables/challenge1.js
+++ Desafios-Entregables/challenge1.js
@@ -1,19 +1,29 @@
+// Clase productManager
 class productManager {
+
+    // Constructor
     constructor () {
+
         this.products = []
+
     }
 
+    // Métodos
     addProduct(title, description, price, thumbnail, code, stock) {
         try {
 
             if (this.products.find(prod => prod.code === code)) {
+
                 console.log(`Ya existe un producto con el código ${code}`);
                 return;
+
             } else if (title && description && price && thumbnail && code && stock) {
+
                 if (this.products.find(prod => prod.code === code)) {
                     console.log(`Ya existe un producto con el código ${code}`);
                     return;
                 } else {
+
                     const product = {
                         id: this.products.length,
                         title: title,
@@ -26,30 +36,44 @@ class productManager {
             
                     this.products.push(product);
                 }
+
             } else {
+
                 console.error('Hay parametros vacíos, asegurate de completar todos')
                 return;
+
             }
         } catch (error) {
+
             console.log(error);
+
         }
 
         return this.products;
     }
     
     getProducts () {
+
         return this.products;
+
     }
     
     getProductById (id) {
+
         return this.products.filter(product => product.id === id);
+
     }
 }
 
+// Crear el objeto productManager
 let pm = new productManager;
+
+// Agregar productos
 pm.addProduct('Maceta', 'Maceta 50 litros', 3000, 'No disponible', 'a1', 10);
 pm.addProduct('Mesa', 'Mesa de centro ratona de cafe', 50000, 'No disponible', 'b2', 25);
 pm.addProduct('Sommier', 'Cama sommier', 80000, 'No disponible', 'c3', 2);
 pm.addProduct('Mesa', 'Mesa de Ping Pong', 50000, 'No disponible', 'd4', 7);
+
+// Obtener productos
 console.log(pm.getProducts());
 console.log(pm.getProductById(2));
