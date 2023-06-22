580fd5cdaef22de5d8d2109cef786cfbea9380e0
ssooftt <71947487+ssooftt@users.noreply.github.com>
2023-06-21 22:11:20 -0300

    Added anticipated files from upcoming challenge

--- Clase4.js
+++ Clase4.js
@@ -3,6 +3,6 @@ const fs = require('fs');
 const d = new Date().toLocaleDateString();
 const t = new Date().toLocaleTimeString();
 
-fs.writeFile('fecha.txt', `Hoy es ${d} y son las ${t}`, (err) => {
-    fs.readFile('fecha.txt', 'utf-8', (err, data) => err ? console.log(err) : console.log(data));
+fs.writeFileSync('fecha.txt', `Hoy es ${d} y son las ${t}`, (err) => {
+    fs.readFileSync('fecha.txt', 'utf-8', (err, data) => err ? console.log(err) : console.log(data));
 });
