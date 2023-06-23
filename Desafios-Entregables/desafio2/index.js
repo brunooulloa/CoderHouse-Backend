c05d66140b8456573e89a42c5827281d79d43307
ssooftt <71947487+ssooftt@users.noreply.github.com>
2023-06-23 02:12:39 -0300

    Challenge `desafio2` completed ✔️

--- Desafios-Entregables/desafio2/index.js
+++ Desafios-Entregables/desafio2/index.js
@@ -2,19 +2,28 @@ const fs = require('fs');
 
 class ProductManager {
     constructor() {
-        this.products = [fs.readFileSync('./products.json', 'utf-8')];
+
+        this.products = JSON.parse(fs.readFileSync('./products.json', 'utf-8'));
+
     }
 
     getProducts() {
+
         return this.products;
+
     }
 
     getProductById (id) {
+
         try {
+
             return this.products.filter(product => product.id === id);
+
         } catch (error) {
+
             console.log('No se encontró el producto');
             console.log(error.message);
+
         }
     }
 
@@ -29,8 +38,10 @@ class ProductManager {
             } else if (title && description && price && thumbnail && code && stock) {
 
                 if (this.products.find(prod => prod.code === code)) {
+
                     console.log(`Ya existe un producto con el código ${code}`);
                     return;
+
                 } else {
 
                     const product = {
@@ -44,7 +55,8 @@ class ProductManager {
                     };
             
                     this.products.push(product);
-                    fs.writeFileSync('./products.json', JSON.stringify(this.products, null, '\t'));
+                    fs.writeFileSync('./products.json', JSON.stringify(this.products, null, 4));
+                    return 'Producto agregado correctamente';
                 }
 
             } else {
@@ -58,32 +70,65 @@ class ProductManager {
             console.log(error.message);
 
         }
-
-        return this.products;
     }
 
-    updateProduct (id, title, description, price, thumbnail, code, stock) {
+    updateProduct (id, ...[title, description, price, thumbnail, code, stock]) {
         try {
+
             const product = {
                 id: id,
-                title: title,
-                description: description,
-                price: price,
-                thumbnail: thumbnail,
-                code: code,
-                stock: stock
+                title: title !== undefined ? title : this.products[id].title,
+                description: description !== undefined ? description : this.products[id].description,
+                price: price !== undefined ? price : this.products[id].price,
+                thumbnail: thumbnail !== undefined ? thumbnail : this.products[id].thumbnail,
+                code: code !== undefined ? code : this.products[id].code,
+                stock: stock !== undefined ? stock : this.products[id].stock
             };
 
             this.products[id] = product;
             fs.writeFileSync('./products.json', JSON.stringify(this.products, null, 4));
 
             return this.products;
+
         } catch (error) {
+
             console.log(error.message);
+
+        }
+    }
+
+    deleteProduct (id) {
+        try {
+
+            if (id < 0) {
+
+                throw new Error('El id no puede ser negativo');
+
+            } else if (this.getProductById(id).length === 0) {
+
+                throw new Error('El producto no existe');
+
+            } else {
+
+                this.products.splice(id, 1);
+                fs.writeFileSync('./products.json', JSON.stringify(this.products, null, 4));
+
+                return 'Producto eliminado correctamente';
+            }
+            
+        } catch (error) {
+
+            console.log(error.message);
+
         }
     }
 }
 
 const pm = new ProductManager();
 
-console.log(pm.addProduct('Maceta 10 lts', 'Maceta de 10 lts', 100, 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.mercadolibre.com.ar%2Fmaceta-de-plastico-50-litros%2Fp%2FMCO15890191&psig=AOvVaw0QZ2Z3Z2Z2Z2Z2Z2Z2Z2Z2&ust=1629789840000000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCJjQ4ZqH9vICFQAAAAAdAAAAABAD', 'MAC50', 10));
+console.log(pm.getProducts());
+console.log(pm.addProduct('producto prueba', 'Este es un producto prueba', 200, 'Sin imagen', 'abc123', 25));
+console.log(pm.getProducts());
+console.log(pm.getProductById(0));
+console.log(pm.updateProduct(0, 'producto prueba 2', undefined, undefined, undefined, undefined, undefined));
+console.log(pm.deleteProduct(0));
