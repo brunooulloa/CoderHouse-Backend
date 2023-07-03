9e2f04d11d9e0a0fceb88482b4b167c025132f47
Soft <brunoulloaduran90@gmail.com>
2023-07-03 19:11:02 -0300

    `Beautified` code

--- Desafios-Entregables/Entrega3/ProductManager.js
+++ Desafios-Entregables/Entrega3/ProductManager.js
@@ -1,5 +1,7 @@
 import fs from 'fs';
+
 export class ProductManager {
+
     constructor(path) {
 
         this.path = path;
@@ -10,6 +12,7 @@ export class ProductManager {
     }
 
     async saveProducts() {
+
         try {
 
             const data = JSON.stringify(this.products);
@@ -21,15 +24,21 @@ export class ProductManager {
             throw new Error('No se pudo guardar el archivo de productos.');
           
         }
+
     }
 
     async loadProducts() {
+
         try {
+
             let data = await fs.promises.readFile(this.path, 'utf-8');
             this.products = JSON.parse(data);
             return this.products;
+
         } catch(error) {
+
             throw new Error('No se pudo leer el archivo de productos, se cargará un arreglo vacío.');
+
         }
     }
 
@@ -40,16 +49,25 @@ export class ProductManager {
     }
 
     async getProductById(id) {
+
         try {
+
             await this.loadProducts();
             let product = this.products.find((product) => product.id === id);
+
             if (!product) {
+
                 console.error('Producto no encontrado');
                 return;
+
             }
+
             return product;
+
         } catch(error) {
+
             throw new Error(error.message);
+
         }
     }
 
@@ -63,6 +81,7 @@ export class ProductManager {
         
         let lastProd = this.products[this.products.length - 1];
         let nextId = 1;
+
         if (lastProd) {
           nextId = lastProduct.id + 1;
         }
@@ -71,25 +90,38 @@ export class ProductManager {
         this.products.push(newProduct);
         await this.saveProducts();
         console.log('Producto agregado:', newProduct);
+
     }
 
     async updateProduct(id, updatedFields) {
+
         let product = await this.getProductById(id);
         if (!product) return;
+
         Object.keys(updatedFields).forEach((key) => {
+
             product[key] = updatedFields[key];
+
         });
+
         await this.saveProducts();
         console.log('Producto actualizado:', product);
+
     }
 
     async deleteProduct(id) {
         let index = this.products.findIndex((product) => product.id === id);
+
         if (index === -1) {
+
           return;
+
         }
+
         this.products.splice(index, 1);
         await this.saveProducts();
         console.log(`Producto con ID ${id} ha sido eliminado.`);
-      }
+
+    }
+
 }
