634fd4c6d0c5ff749eb0be308dbbfb2655260fa2
Soft <brunoulloaduran90@gmail.com>
2023-07-11 10:08:01 -0300

    Changed folder name from `controller` to `controllers`

--- Desafios-Entregables/Entrega4/src/routes/carts.routes.js
+++ Desafios-Entregables/Entrega4/src/routes/carts.routes.js
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { CartManager } from '../controller/CartManager.js';
+import { CartManager } from '../controllers/CartManager.js';
 
 const router = Router();
 const cm = new CartManager(`../../data/carts.json`);
