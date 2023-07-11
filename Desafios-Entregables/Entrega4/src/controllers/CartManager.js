c0f9d8d5088e39689875576bfea5188eef45f922
Soft <brunoulloaduran90@gmail.com>
2023-07-11 12:55:44 -0300

    Re-formatted code ✨

--- Desafios-Entregables/Entrega4/src/controllers/CartManager.js
+++ Desafios-Entregables/Entrega4/src/controllers/CartManager.js
@@ -4,189 +4,118 @@ import { __dirname } from '../utils.js';
 import { v4 as uuidv4 } from 'uuid';
 
 export class CartManager {
-
     constructor(path) {
-
         this.path = pathc.join(__dirname, 'data', path);
         this.loadCarts().then(() => console.log('Carts loaded.'));
-
     }
 
     existsFile() {
-
         return fs.existsSync(this.path);
-
     }
 
     async saveCarts() {
-
         try {
-
             const data = JSON.stringify(this.carts, null, 4);
             await fs.promises.writeFile(this.path, data);
             return 'Archivo de carritos guardado.';
-
         } catch(error) {
-
             throw error('No se pudo guardar el archivo de carritos.');
-          
         }
-
     }
 
     async save() {
-
         try {
-
             if (this.existsFile()) {
-
                 const newCart = {
-
                     id: this.generateId(),
                     products: []
-
                 }
 
                 this.carts.push(newCart);
                 await this.saveCarts();
                 return 'Archivo de carritos guardado.';
             } else {
-
                 this.carts = [];
                 await this.saveCarts();
                 throw new Error('No se encontro el archivo de carritos, se guardara un arreglo vacío.');
-
             }
-
         } catch(error) {
-
             throw error('No se pudo guardar el archivo de carritos.');
-          
         }
-
     }
 
     generateId() {
-
         return uuidv4();
-
     }
 
     async loadCarts() {
-
         try {
-
             if (this.existsFile()) {
-
                 let data = await fs.promises.readFile(this.path, 'utf-8');
                 this.carts = JSON.parse(data);
                 return this.carts;
-
             } else {
-
                 this.carts = [];
                 throw new Error('No se encontro el archivo de carritos, se cargará un arreglo vacío.');
-
             }
-            
-
         } catch(error) {
-
             throw error('No se pudo leer el archivo de carritos, se cargará un arreglo vacío.');
-
         }
-
     }
 
     getCarts() {
-
         return this.carts;
-
     }
 
     async getCartById(id) {
-
         try {
-
             await this.loadCarts();
             let cart = this.carts.find((cart) => cart.id == id);
 
             if (!cart) throw new Error('Carrito no encontrado');
 
             return cart;
-
         } catch(error) {
-
             throw error(error.message);
-
         }
 
     }
 
     async addCart(cart) {
-
         try {
-        
-            if (!cart || !cart.products) {
-
-                throw error('Fields validation failed!');
-
-            }
-        
+            if (!cart || !cart.products) throw error('Fields validation failed!');
             const newCart = { id: this.generateId, ... cart };
             this.carts.push(newCart);
             await this.saveCarts();
             console.log('Carrito agregado:', newCart);
-        
         } catch(error) {
-            
             throw error(error.message);
-        
         }
-
     }
 
     async updateCart(cid, pid) {
-
         try {
-
             let cart = await this.getCartById(cid);
             if (!cart) return;
-
             Object.keys(updatedFields).forEach((key) => {
-
                 cart[key] = updatedFields[key];
-
             });
-
             await this.saveCarts();
             console.log('Carrito actualizado:', cart);
-        
         } catch(error) {
-
             throw error(error.message);
-
         }
-
     }
 
     async deleteProduct(id) {
-
         try {
-
             let index = this.carts.findIndex((cart) => cart.id === id);
             if (index === -1) return;
-
             this.carts.splice(index, 1);
             await this.saveCarts();
             console.log(`Carrito con ID ${id} ha sido eliminado.`);
-
         } catch(error) {
-
             throw error(error.message);
-
         }
-
     }
-
 }
--- Desafios-Entregables/Entrega4/src/routes/carts.routes.js
+++ Desafios-Entregables/Entrega4/src/routes/carts.routes.js
@@ -5,136 +5,86 @@ const router = Router();
 const cm = new CartManager(`../../data/carts.json`);
 
 const fieldsCheck = (req, res, next) => {
-
     let cart = req.body;
-
-    if (!cart.products) {
-
-        return res.json({ status: 'error', message: 'Debe ingresar todos los parametros' });
-
-    } else {
-
-        next();
-        
-    }
+    if (!cart.products) return res.json({ status: 'error', message: 'Debe ingresar todos los parametros' })
+    else next();
 };
 
 router.post('/', async (req, res) => {
-
     try {
-
         let cartCreated = await cm.save();
         res.json({ status: 'success', data: cartCreated });
-
     } catch (error) {
-
         res.json({ status: 'error', message: error.message });
         throw error(error.message);
-
     }
-
 });
 
 router.get('/', async (req, res) => {
-
     try {
-
         let limit = Number(req.query.limit);
-
         if (!limit) {
-
             let result = await cm.getCarts();
             res.send(result);
-
         } else {
-
             let result = await cm.getCarts();
             res.send(result.slice(0, limit));
-
         }
-
     } catch (error) {
-
         res.json({ status: 'error', message: error.message });
         throw error(error.message);
-
     }
-
 });
 
 router.get('/:cid', async (req, res) => {
-
     try {
-
         let cid = req.params.cid;
         let result = await cm.getCartById(cid);
         res.send(result);
-
     } catch (error) {
-
         res.json({ status: 'error', message: error.message });
         throw error(error.message);
-
     }
-
 });
 
 router.put('/:cid', fieldsCheck, async (req, res) => {
-
     try {
-
         let cid = req.params.cid;
         let cart = req.body;
         let result = await cm.updateCart(cid, cart);
         result.id = cid;
         res.send(result);
-
     } catch (error) {
-
         res.json({ status: 'error', message: error.message });
         throw error(error.message);
-
     }
-    
 });
 
 router.post('/:cid/products/:pid', async (req, res) => {
-    
     try {
-
         let cid = req.params.cid;
         let pid = req.params.pid;
         let cart = await cm.getCartById(cid);
         let prods = cart.products;
         let isProdInCart = prods.find(p => p.id == pid)
-
         if (isProdInCart) {
-            
             let index = prods.findIndex(p => p.id == pid);
             cart.products[ index ].quantity++;
             cm.saveCarts();
             res.json({ status: 'success', data: cart });
-
         } else {
-
             let newProd = {
                 product: pid,
                 quantity: 1
             }
-
             cart.products.push(newProd);
             cm.saveCarts()
             res.json({ status: 'success', data: cart });
-
         }
-
     } catch (error) {
-
         res.json({ status: 'error', message: error.message });
-        throw error(error.message);
-
-    }
-        
+        throw error(error.message);f
+    } 
 });
 
 export { router as cartsRouter };
